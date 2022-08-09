6b038319a1c65d461dafc24ee7621585fdb43f4c
samfoucart <samfoucart25@gmail.com>
2022-08-09 10:56:13 -0400

    Added Stub Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import './Home.css';
 
 export default function Home() {
     return (
-        <div>
-            <p>Home</p>
+        <div className="home-container">
+            <h1>Hello!</h1>
+            <p>I am Sam Foucart, a computer engineer from southeast Ohio. I am currently working at NICE CXone on the CXone recording platform.</p>
+            <p>This website is a home for my engineering blog and side projects. It was originally made to learn React, but I have been adding more too it as time goes on.
+                Although I'm not formally a frontend developer, this still shows a demonstration of my work.
+            </p>
         </div>
     )
 }
