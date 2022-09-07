3aa20922306245e6accbfbf9f7322be64af6e715
samfoucart <samfoucart25@gmail.com>
2022-09-07 10:25:26 -0400

    Removed Link

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import './Projects.css'
 
 const projectDatabase: IProjectHeader[] = [{
