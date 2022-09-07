93268934c94bffcab90d8b669dfa5cbe5cd9c6b3
samfoucart <samfoucart25@gmail.com>
2022-09-07 10:25:00 -0400

    Projects references external urls

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -7,6 +7,7 @@ const projectDatabase: IProjectHeader[] = [{
         description: 'A lit, shaded mesh that over time approaches a steady state temperature. This was my first project made in WebGL and was completed as the final project of CS4250 of Ohio University.',
         image: '/images/projects/mesh_thumb.png',
         url: 'fourier_heat_equation_interactive_webgl_demonstation',
+        outside_url: 'https://samfoucart.github.io/Fourier-Series-Visualization/index.html',
         id: 1
     },
     {
@@ -14,6 +15,7 @@ const projectDatabase: IProjectHeader[] = [{
         description: 'A randomly generated maze and a controllable mouse that is able to solve the maze. This project was made during my sophomore year at Ohio University as part of IEEE.',
         image: '/images/projects/micromouse_thumb.png',
         url: 'micromouse_maze_solving_demonstration',
+        outside_url: 'https://samfoucart.github.io/Ohio-University-Micromouse/simulator/index.html',
         id: 2
     },
     {
@@ -21,6 +23,7 @@ const projectDatabase: IProjectHeader[] = [{
         description: 'An Android application that is able to collect, plot, and save data from an Ocean Optics USB2000 spectrometer. This project was made as part of the PACE program at Ohio University.',
         image: '/images/projects/zoomed-spectra.png',
         url: 'electromagnetic_spectral_data_collector',
+        outside_url: 'https://github.com/samfoucart/oueecs_SpectralDataViewer',
         id: 3
     }
 ]
@@ -50,7 +53,7 @@ export default function Projects() {
                     <div className="project-metadata">
                         {unformattedProjects.map((project: IProjectHeader) => {
                             return (
-                                <Link to={`/projects/${project.url}`} key={project.id}>
+                                <a href={project.outside_url? `${project.outside_url}` : `${process.env.PUBLIC_URL}/projects/${project.url}`} key={project.id}>
                                     <div className="project-outline">
                                         <img src={process.env.PUBLIC_URL + project.image} alt={project.title} width="128" height="128"/>
                                         <div className="project-text">
@@ -58,7 +61,7 @@ export default function Projects() {
                                             <p>{project.description}</p>
                                         </div>
                                     </div>
-                                </Link>
+                                </a>
                             );
                         })}
                     </div>
@@ -79,5 +82,6 @@ interface IProjectHeader {
     description: string,
     image: string,
     url: string,
-    id: number
+    id: number,
+    outside_url?: string,
 }
